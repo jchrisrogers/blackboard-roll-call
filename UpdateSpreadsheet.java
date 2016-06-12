6459b010640f97d23c29406cf1c7124010a58bba
tuyenle92 <tuyen_le92@rocketmail.com>
2016-06-11 18:33:01 -0700

    Update UpdateSpreadsheet.java

--- UpdateSpreadsheet.java
+++ UpdateSpreadsheet.java
@@ -3,22 +3,23 @@ package mypackage;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.*;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
+import com.google.common.collect.Lists;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.*;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 import com.google.gdata.util.ServiceException;
+import com.google.api.services.content.ShoppingContent;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -26,10 +27,10 @@ import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.PrivateKey;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+import static com.google.common.collect.Lists.newArrayList;
+
 
 /** Update empty sheet **/
 public class UpdateSpreadsheet  {
@@ -184,14 +185,70 @@ public class UpdateSpreadsheet  {
                         .set("Name", "tuyen")).execute();
     }*/
 
+
+
+    private static void setUp() throws GeneralSecurityException, IOException {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+        // Go to the Google Developers Console, open your application's
+        // credentials page, and copy the client ID and client secret.
+        // Then paste them into the following code.
+        GoogleClientSecrets.Details details = new GoogleClientSecrets.Details();
+        details.setClientId("532786746523-n7r8fv173dc96kfmlo59bfd8cn5mmh0c.apps.googleusercontent.com");
+        details.setClientSecret("yu3ZgNxt7grMItEx-_LZWuAB");
+
+        // Or your redirect URL for web based applications.
+        String redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+        String scope = "https://www.googleapis.com/auth/content";
+
+        GoogleClientSecrets clientSecrets = new GoogleClientSecrets();
+        clientSecrets.setInstalled(details);
+
+        GoogleAuthorizationCodeFlow authorizationFlow = new GoogleAuthorizationCodeFlow
+                .Builder(httpTransport, jsonFactory, clientSecrets, newArrayList(scope))
+                .setAccessType("offline").build();
+
+        String authorizeUrl =
+                authorizationFlow.newAuthorizationUrl().setRedirectUri(redirectUrl).build();
+        System.out.println("Paste this url in your browser: \n" + authorizeUrl + '\n');
+
+        // Wait for the authorization code.
+        System.out.println("Type the code you received here: ");
+        String authorizationCode = new BufferedReader(new InputStreamReader(System.in)).readLine();
+
+        // Authorize the OAuth2 token.
+        GoogleAuthorizationCodeTokenRequest tokenRequest =
+                authorizationFlow.newTokenRequest(authorizationCode);
+        tokenRequest.setRedirectUri(redirectUrl);
+        GoogleTokenResponse tokenResponse = tokenRequest.execute();
+
+        // Create the OAuth2 credential.
+        GoogleCredential credential = new GoogleCredential.Builder()
+                .setTransport(new NetHttpTransport())
+                .setJsonFactory(new JacksonFactory())
+                .setClientSecrets(clientSecrets)
+                .build();
+
+        // Set authorized credentials.
+        credential.setFromTokenResponse(tokenResponse);
+
+        ShoppingContent service = new ShoppingContent
+                .Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("YOUR_APPLICATION_NAME")
+                .build();
+    }
+
+
     public static void main(String[] argv) throws IOException, ServiceException, GeneralSecurityException {
+        setUp();
         /*Sheets service = updateSheetService();
        service.spreadsheets().values().batchUpdate(SPREADSHEET_ID,
                 new BatchUpdateValuesRequest()
                         .setValueInputOption("RAW").setData()*/
 
 
-        List<Request> requests = new ArrayList<>();
+        /*List<Request> requests = new ArrayList<>();
         Sheets service = updateSheetService();
         String spreadsheetId = SPREADSHEET_ID;
         requests.add(new Request()
@@ -217,7 +274,7 @@ public class UpdateSpreadsheet  {
                 .setRequests(requests);
         service.spreadsheets().batchUpdate(spreadsheetId, batchUpdateRequest)
                 .execute();
-
+       */
     }
 
 
