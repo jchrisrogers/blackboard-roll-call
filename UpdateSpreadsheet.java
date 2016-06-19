7ff4803016d8e4672787d2e6072c2a38062153c0
tuyenle92 <tuyen_le92@rocketmail.com>
2016-06-19 00:29:30 -0700

    Update UpdateSpreadsheet.java

--- UpdateSpreadsheet.java
+++ UpdateSpreadsheet.java
@@ -1,129 +1,19 @@
 package mypackage;
 
-import com.google.api.client.auth.oauth2.BearerToken;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.*;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
-import com.google.gdata.data.dublincore.Date;
 import com.google.gdata.data.spreadsheet.*;
 import com.google.gdata.util.ServiceException;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
-import java.io.FileInputStream;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class UpdateSpreadsheet {
-    /**
-     * Id of spreadsheet
-     **/
-    private static String SPREAD_SHEET_ID = "19UE63tTGZ9w5Tid00IFKwWwvxM0G8Zn2HFKjhqlFm6g";
-
-
-    /**
-     * Application name.
-     */
-    private static final String APPLICATION_NAME =
-            "Google Sheets API Java Quickstart";
-
-    /**
-     * Directory to store user credentials for this application.
-     */
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-            System.getProperty("user.home"), ".credentials//sheets.googleapis.com-java-quickstart.json");
-
-    /**
-     * Global instance of the {@link FileDataStoreFactory}.
-     */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    /**
-     * Global instance of the JSON factory.
-     */
-    private static final JsonFactory JSON_FACTORY =
-            JacksonFactory.getDefaultInstance();
-
-    /**
-     * Global instance of the HTTP transport.
-     */
-    private static HttpTransport HTTP_TRANSPORT;
-
-    /**
-     * Global instance of the scopes required by this quickstart.
-     * <p>
-     * If modifying these scopes, delete your previously saved credentials
-     * at ~/.credentials/sheets.googleapis.com-java-quickstart.json
-     */
-    private static final List<String> SCOPES =
-            Arrays.asList(SheetsScopes.SPREADSHEETS);
-
-    static {
-        try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(1);
-        }
-    }
-
-    /**
-     * Creates an authorized Credential object.
-     *
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize() throws IOException {
-        // Load client secrets.
-        InputStream in = new FileInputStream("/home/tuyen/IdeaProjects/application/src/main/resources/client_secret.json");
-        // SheetsQuickstart.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                        .setDataStoreFactory(DATA_STORE_FACTORY)
-                        .setAccessType("offline")
-                        .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-                flow, new LocalServerReceiver()).authorize("user");
-        System.out.println(
-                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-        return credential;
-    }
-
-    /**
-     * Build and return an authorized Sheets API client service.
-     *
-     * @return an authorized Sheets API client service
-     * @throws IOException
-     */
-    public static Sheets getSheetsService() throws IOException {
-        Credential credential = authorize();
-        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
-
+public class UpdateSpreadsheet extends EmptySheet {
 
     /**
      * Return the number of row in
@@ -176,7 +66,7 @@ public class UpdateSpreadsheet {
 
         // Check for user input. Make sure user input first and last name with their correct ID number
         if (isInputValid(name, id)) {
-            
+
             // Build a new authorized API client service.
             Sheets service = getSheetsService();
 
@@ -226,7 +116,7 @@ public class UpdateSpreadsheet {
             // Final call to publish the updated sheet to google drive
             BatchUpdateSpreadsheetRequest batchUpdateRequest = new BatchUpdateSpreadsheetRequest()
                     .setRequests(requests);
-            service.spreadsheets().batchUpdate(SPREAD_SHEET_ID, batchUpdateRequest)
+            service.spreadsheets().batchUpdate(getSpreadsheetID(), batchUpdateRequest)
                     .execute();
 
             // Reset to true if user input is correct. Otherwise false
@@ -326,7 +216,7 @@ public class UpdateSpreadsheet {
         String range = "Form Responses 1!A2:D";
 
         // Get range
-        return  service.spreadsheets().values().get(SPREAD_SHEET_ID, range).execute();
+        return  service.spreadsheets().values().get(getSpreadsheetID(), range).execute();
     }
 
 
